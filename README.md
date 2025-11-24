dacfe9ea98b460984db4252ae1efc39783ba3845
Azzam S.A <noreply@azzamsa.com>
2025-11-24 18:01:11 +0700

    feat: add super-productivity

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ Words from various sources
 - [Slime words][2]. GPL v2.
 - [Doom Emacs][3]. MIT.
 - [Github][4] [Zen][5]
+- [Super-Productivity][6]
 
 
 [1]: https://github.com/freeCodeCamp/freeCodeCamp/blob/52c99823b84401cb7c173efb6020887f2d7d8be6/common/app/utils/words.json
@@ -15,3 +16,4 @@ Words from various sources
 [3]: https://github.com/doomemacs/doomemacs/blob/58fb83c98eac32fc77710ee2f6bcfb3a86f7b6fb/modules/ui/doom-quit/config.el
 [4]: https://docs.github.com/en/rest/meta/meta?apiVersion=2022-11-28#get-the-zen-of-github
 [5]: https://ben.balter.com/2015/08/12/the-zen-of-github/
+[6]: https://github.com/johannesjo/super-productivity/blob/43914ac84f72652e4f9f47934cad60be9d383cc2/src/index.html#L248
---
+++ super-productivity/quotes.js
@@ -0,0 +1,104 @@
+var productivityTips = [
+    [
+        'There is only one solution to complex problems',
+        'Try and correct, try and correct, try and correct, try and correct, try and correct...',
+    ],
+    [
+        '2 Step Super Productivity',
+        '1. Physically get in your work position. 2. Do one small thing.',
+    ],
+    ['Prioritize', 'Do first things first.'],
+    [
+        'Procrastination',
+        'Procrastination is an emotion regulation problem, not a time management problem.',
+    ],
+    [
+        'π',
+        'When in doubt use the magic programmers estimation factor to calculate time estimates. Programmers estimate times π.',
+    ],
+    ['Be realistic', 'Almost everything will take longer than you think.'],
+    [
+        'Good is good enough',
+        'Don`t aim for perfection. Focus on the important things.',
+    ],
+    [
+        'Distractions',
+        'If something is distracting you, get rid of it. If you can`t get it out of your head and you can deal with it later, write it down.',
+    ],
+    ['Fun things first', 'The dopamine can get you through the rest.'],
+    [
+        'Celebrate!',
+        'After completing a task, don`t rush to the next! Give yourself some time to feel good about yourself.',
+    ],
+    [
+        'Establish Routines',
+        'Decisions cost time. So establishing routines can be very helpful.',
+    ],
+    [
+        'Take breaks',
+        'They help you clear your mind and make you almost always more productive in the long run. Go! Take a walk!',
+    ],
+    ['Mess creates stress', 'Clean up your desk!'],
+    ['Focus on a single task', 'Multi-tasking is the worst.'],
+    [
+        'Set your goals',
+        'Having a goal to aim for (or even some smaller ones) increases motivation and focus. Remind yourself why you are doing something.',
+    ],
+    [
+        'Try to get it right the first time',
+        'This can help you to avoid dragging a task on forever.',
+    ],
+    [
+        'We are what we repeatedly do.',
+        'Excellence, then, is not an act, but a habit. – Aristotle',
+    ],
+    [
+        'You don’t learn to walk by following rules.',
+        'You learn by doing, and by falling over. – Richard Branson',
+    ],
+    ['A problem well-put is half-solved', '― John Dewey'],
+    [
+        "Either like what you do or know why you're doing it.",
+        'If not, consider doing something else.',
+    ],
+    ['Wisdom is learning what to overlook', '― William James'],
+    ['Better to do it than live with the fear of it.', '― Logen Ninefingers'],
+    [
+        'It is not that I’m so smart',
+        'But I stay with the questions much longer – Albert Einstein',
+    ],
+    ['No problem can withstand the assault of sustained thinking', '― Voltaire'],
+    [
+        "I have not failed. I've just found 10,000 ways that won't work.",
+        '― Thomas A. Edison',
+    ],
+    [
+        'Review and Reflect',
+        "At the end of the day or week, review your progress and reflect on what worked and what didn't. Adjust your strategies accordingly.",
+    ],
+    [
+        'Set Realistic Expectations',
+        "Don't overburden yourself with an unrealistic workload. Be honest about what you can accomplish in a given time frame.",
+    ],
+    [
+        'Prioritize Tasks',
+        "Use techniques like the Eisenhower Matrix (urgent vs. important) to prioritize your tasks. Focus on what's most important and time-sensitive.",
+    ],
+    [
+        'Always be kind to yourself',
+        'Productivity is impossible without good self care (at the very least in the long run).',
+    ],
+    [
+        'The path to progress is all about self evaluation',
+        'From time to time you should reflect if your goals and methods serve you well.',
+    ],
+    [
+        'Embrace Progress over Achievement',
+        'It’s not about massive strides every day but the accumulation of small, incremental progress that leads to significant achievements over time.',
+    ],
+    ["You don't need more time in your day. You need to decide.", '― Seth Godin'],
+    [
+        'Decide to decide what not to do',
+        'There is only is so much time in your day (and in your life), you need to decide what you really want to do.',
+    ],
+];
